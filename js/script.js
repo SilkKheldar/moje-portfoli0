b1d91ba9ee1f3aca65d006b370877da0ddda713f
Tomasz Krupinski <tomasz.k.krupinski@gmail.com>
2019-12-12 16:41:30 +0100

    po odcinku 3.2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
   $("#slides").superslides({
     animation: "fade",
-    play: 5000,
+    play: 10000,
     pagination: false
+  });
+
+  var typed = new Typed(".typed", {
+    strings: ["Solution Designer.", "Software Engineer.", "Business Analyst."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
   });
 });
